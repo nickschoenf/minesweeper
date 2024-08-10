6f7228589c7d9213aaf57e60f2b173cc83dde6a6
Nick <ultranick8@gmail.com>
2024-08-10 02:37:20 -0400

    Implemented visuals

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy::input::mouse::MouseButton;
+use bevy::input::ButtonInput;
+use bevy::sprite::ColorMaterial;
 
 struct Board {
     tiles: Vec<Tile>,
@@ -90,6 +94,23 @@ impl Board {
     }
 }
 
+// Tile component
+#[derive(Component)]
+struct DisplayTile {
+    x: u32,
+    y: u32,
+}
+
+// Plugin to set up the game
+pub struct TileGridPlugin;
+
+impl Plugin for TileGridPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_tiles)
+            .add_systems(Update, tile_click_system);
+    }
+}
+
 #[derive(Clone, Copy)]
 struct Tile {
     is_mine: bool,
@@ -124,33 +145,91 @@ fn main() {
     println!("{}", board.to_string());
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, spawn_tiles)
+        .add_systems(Update, tile_click_system)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(NodeBundle {
-        style: Style {
-            width: Val::Percent(100.),
-            ..default()
-        },
-        ..default()
-    })
-    .with_children(|root| {
-        // Text where we display current resolution
-        root.spawn((
-            ImageBundle {
-                style: Style {
-                    ..default()
-                },
-                image: UiImage::new(asset_server.load("tile_covered.png")),
-                ..default()
-            },
-        ));
-    });
-}
+
+// fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+//     commands.spawn(Camera2dBundle::default());
+//     commands.spawn(NodeBundle {
+//         style: Style {
+//             width: Val::Percent(100.),
+//             ..default()
+//         },
+//         ..default()
+//     })
+//     .with_children(|root| {
+//         // Text where we display current resolution
+//         root.spawn((
+//             ImageBundle {
+//                 style: Style {
+//                     ..default()
+//                 },
+//                 image: UiImage::new(asset_server.load("tile_covered.png")),
+//                 ..default()
+//             },
+//         ));
+//     });
+// }
 
 fn fail() {
     println!("You lost!");
+}
+
+fn spawn_tiles(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let tile_size = 64.0;
+    let grid_width = 8;
+    let grid_height = 8;
+
+    for y in 0..grid_height {
+        for x in 0..grid_width {
+            commands.spawn((
+                SpriteBundle {
+                    texture: asset_server.load("tile_covered.png"),
+                    transform: Transform::from_xyz(
+                        x as f32 * tile_size,
+                        y as f32 * tile_size,
+                        0.0,
+                    ),
+                    ..default()
+                },
+                DisplayTile { x: x as u32, y: y as u32 },
+            ));
+        }
+    }
+}
+
+fn tile_click_system(
+    buttons: Res<ButtonInput<MouseButton>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut tile_query: Query<(Entity, &Transform, &DisplayTile, &mut Sprite)>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let window = window_query.single();
+    
+    if let Some(cursor_position) = window.cursor_position() {
+        if buttons.just_pressed(MouseButton::Left) {
+            let (camera, camera_transform) = camera_query.single();
+            
+            if let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) {
+                let world_position = ray.origin.truncate();
+                for (entity, transform, tile, mut sprite) in tile_query.iter_mut() {
+                    let tile_pos = transform.translation.truncate();
+                    let tile_size = 64.0;
+                    let tile_min = tile_pos - Vec2::splat(tile_size / 2.0);
+                    let tile_max = tile_pos + Vec2::splat(tile_size / 2.0);
+                    
+                    if world_position.cmpge(tile_min).all() && world_position.cmple(tile_max).all() {
+                        println!("Clicked tile at ({}, {})", tile.x, tile.y);
+                        // Change the tile color to red
+                        sprite.color = Color::RED;
+                    }
+                }
+            }
+        }
+    }
 }
